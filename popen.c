a2742225c335c73304d244e489a1042d77ad8282
Alex-IA69 <A01635557@tec.mx>
2022-10-13 13:18:17 +0000

    Practica 4

---
+++ popen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    // popen corre un programa 
+    // en este caso lee un archivo y escribe el otro
+    FILE *lsOutput;
+    FILE *tomayIntput;
+    char readBuffer[80];
+    lsOutput = popen("ls", "r"); // read a este archivo
+    tomayIntput = popen("./tomay", "w"); // write a este archivo
+    while(fgets(readBuffer, 80, lsOutput)){ // lee una cadena de archivos del read buffer, tamaño 80 (c) y lo lee de lsOutput
+        fputs(readBuffer, tomayIntput); // escribe una cadena de archivos del readBuffer a tomayIntput
+    }
+    pclose(lsOutput);
+    pclose(tomayIntput);
+}
